50c4fe807fc83b5722d7d0dc7917635069ca72c2
KBENBENECK <kbenbeneck@gmail.com>
2021-05-20 23:51:59 -0400

    Install axios and create config object

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyC8QBeOYSM3FUHpC2b69bbzkY78GyvdGQM';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: '5',
+        key: KEY
+
+    }
+});
