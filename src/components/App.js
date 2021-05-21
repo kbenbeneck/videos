67851267d5aff45a3f130dc2b3c3cd66f34d2fb2
KBENBENECK <kbenbeneck@gmail.com>
2021-05-21 09:32:06 -0400

    Create VideoList fx component. Pass App's videos state to VideoList as props.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
     state = { videos: [] };
@@ -12,14 +13,14 @@ class App extends React.Component {
         });
 
         this.setState({ videos: response.data.items})
-        console.log(response.data.items)
+        
     };
 
     render() {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                {this.state.videos.length}
+                <VideoList videos={this.state.videos}/>
             </div>
         ) 
     }
