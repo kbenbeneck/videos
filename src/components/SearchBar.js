d048f33affcd9bc70ec87f46431312b49a3c62c8
KBENBENECK <kbenbeneck@gmail.com>
2021-05-20 23:31:20 -0400

    Initialize search term state. Add onInputChange and onFormSubmit functions.
    SearchBar is now a controlled component.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value })
+    };
+    
+    onFormSubmit = (event) => {
+        event.preventDefault()
+        //search API
+        //callback from parent comp. 
+    };
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
-                        <input type="text" />
+                        <input 
+                            value={this.state.term} 
+                            type="text" 
+                            onChange={this.onInputChange}
+                        />
                     </div>
                 </form>
             </div>
