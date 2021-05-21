b4dbc3df217759cdf50f1e97d84bee6af6afb730
KBENBENECK <kbenbeneck@gmail.com>
2021-05-21 10:14:07 -0400

    Pass onvideoSelect callback as props through VideoList, to VideoItem. On click, invoke callback and update selectedVideo state in App.

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,28 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { 
+        videos: [],
+        selectedVideo: null
+    };
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+        this.setState({ videos: response.data.items}) 
+    };
 
-        this.setState({ videos: response.data.items})
-        
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video});
     };
 
     render() {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoList videos={this.state.videos}/>
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         ) 
     }
