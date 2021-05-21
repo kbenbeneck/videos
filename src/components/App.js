12af66eadbdee1519ab6bf0fc9f73a5a5e60cfcb
KBENBENECK <kbenbeneck@gmail.com>
2021-05-21 09:28:34 -0400

    Initialize videos array state in App. Update videos array with response object

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        youtube.get('/search', {
+    state = { videos: [] };
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+
+        this.setState({ videos: response.data.items})
+        console.log(response.data.items)
     };
 
     render() {
         return(
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+                {this.state.videos.length}
             </div>
         ) 
     }
