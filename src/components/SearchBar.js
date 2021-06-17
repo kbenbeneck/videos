d3539287cf498c8d30f50ef5fb2b8a4b73be16e3
KBENBENECK <kbenbeneck@gmail.com>
2021-06-17 12:47:59 -0400

    ready for deploy

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = ({ onFormSubmit }) => {
         <div className="search-bar ui segment">
             <form onSubmit={onSubmit} className="ui form">
                 <div className="field">
-                    <label>Video Search</label>
+                    <label>Search For A Video</label>
                     <input 
                         value={term} 
                         type="text" 
